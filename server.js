ac25ebbe98e4dabde73f877471339171a34510f6
Tyrese1 <obatarhe.otughwor@gmail.com>
2019-01-09 01:38:21 +0100

    created enpoint for political party CRUD

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 import express from 'express';
+import politicalParty from './src/apiJsObject/controller/politicalParty';
 
 const app = express()
 
 app.use(express.json())
 
 app.get('/', (req, res) => {
-  return res.status(200).send({'message': 'Server is live and ruuning!'});
+  return res.status(200).send({'message': 'Server is live and running!'});
 })
 
+
+app.post('/api/v1/politicalPartys', politicalParty.create);
+app.get('/api/v1/politicalPartys', politicalParty.getAll);
+app.get('/api/v1/politicalPartys/:id', politicalParty.getOne);
+app.put('/api/v1/politicalPartys/:id', politicalParty.update);
+app.delete('/api/v1/politicalPartys/:id', politicalParty.delete);
 
 
 app.listen(3000)
