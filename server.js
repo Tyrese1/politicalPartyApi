13e87be8eef6775a927b191aab5d9008a830927b
Tyrese1 <obatarhe.otughwor@gmail.com>
2019-01-09 11:00:45 +0100

    refactor Json object to database

--- server.js
+++ server.js
@@ -1,5 +1,12 @@
 import express from 'express';
-import politicalParty from './src/apiJsObject/controller/politicalParty';
+import dotenv from 'dotenv';
+import 'babel-polyfill';
+
+import politicalPartyApiJSObject from './src/apiJsObject/controller/politicalParty';
+import politicalPartyApiDatabase from './src/apiJsonDb/controller/politicalParty';
+
+dotenv.config();
+const  politicalParty = process.env.TYPE === 'db' ?  politicalPartyApiDatabase :  politicalPartyApiJSObject;
 
 const app = express()
 
@@ -9,12 +16,13 @@ app.get('/', (req, res) => {
   return res.status(200).send({'message': 'Server is live and running!'});
 })
 
+//API endpoints
+app.post('/api/v1/politicalPartys', politicalPartyApiDatabase.create);
+app.get('/api/v1/politicalPartys', politicalPartyApiDatabase.getAll);
+app.get('/api/v1/politicalPartys/:id', politicalPartyApiDatabase.getOne);
+app.put('/api/v1/politicalPartys/:id', politicalPartyApiDatabase.update);
+app.delete('/api/v1/politicalPartys/:id', politicalPartyApiDatabase.delete);
 
-app.post('/api/v1/politicalPartys', politicalParty.create);
-app.get('/api/v1/politicalPartys', politicalParty.getAll);
-app.get('/api/v1/politicalPartys/:id', politicalParty.getOne);
-app.put('/api/v1/politicalPartys/:id', politicalParty.update);
-app.delete('/api/v1/politicalPartys/:id', politicalParty.delete);
 
 
 app.listen(3000)
--- src/apiJsObject/model/politicalParty.js
+++ src/apiJsObject/model/politicalParty.js
@@ -1,6 +1,6 @@
 import moment from 'moment';
 import uuidv4 from 'uuid/v4';
-import db from '../db';
+//import db from '../db';
 
 const politicalParty  = {
   /**
@@ -12,13 +12,11 @@ const politicalParty  = {
   async create(req, res) {
     const createQuery = `INSERT INTO
       politicalPartys(id, politicalParty, candidateName, offices, created_date, modified_date)
-      VALUES($1, $2, $3, $4, $5, $6) returning *`;
-
+      VALUES($1, $2, $3, $4, $5) returning *`;
     const values = [
       uuidv4(),
-      req.body.politicalParty,  
-      req.body.candidateName,
-      req.body.offices,
+      req.body.politicalParty,      
+      req.user.id,
       moment(new Date()),
       moment(new Date())
       
@@ -40,9 +38,9 @@ const politicalParty  = {
    * @returns {object} politicalPartys array
    */
   async getAll(req, res) {
-    const findAllQuery = 'SELECT * FROM politicalPartys';
+    const findAllQuery = 'SELECT * FROM politicalPartys WHERE id = $1';
     try {
-      const { rows, rowCount } = await db.query(findAllQuery);
+      const { rows, rowCount } = await db.query(findAllQuery, [req.user.id]);
       return res.status(200).send({ rows, rowCount });
     } catch(error) {
       return res.status(400).send(error);
@@ -52,14 +50,14 @@ const politicalParty  = {
    * Get A politicalParty 
    * @param {object} req 
    * @param {object} res
-   * @returns {object} politicalParty object
+   * @returns {object} politicalParty  object
    */
   async getOne(req, res) {
-    const text = 'SELECT * FROM politicalPartys WHERE id = $1';
+    const text = 'SELECT * FROM politicalPartys WHERE id = $1 AND id = $2';
     try {
-      const { rows } = await db.query(text, [req.params.id]);
+      const { rows } = await db.query(text, [req.params.id, req.user.id]);
       if (!rows[0]) {
-        return res.status(404).send({'message': 'political Party not found'});
+        return res.status(404).send({'message': 'politicalParty  not found'});
       }
       return res.status(200).send(rows[0]);
     } catch(error) {
@@ -75,19 +73,18 @@ const politicalParty  = {
   async update(req, res) {
     const findOneQuery = 'SELECT * FROM politicalPartys WHERE id=$1';
     const updateOneQuery =`UPDATE politicalPartys
-      SET politicalParty=$1, candidateName=$2, offices=$3,modified_date=$4
-      WHERE id=$5 returning *`;
+      SET politicalParty=$1,modified_date=$2
+      WHERE id=$3 returning *`;
     try {
-      const { rows } = await db.query(findOneQuery, [req.params.id]);
+      const { rows } = await db.query(findOneQuery, [req.params.id, req.user.id]);
       if(!rows[0]) {
-        return res.status(404).send({'message': 'Political Party not found'});
+        return res.status(404).send({'message': 'politicalParty not found'});
       }
       const values = [
         req.body.politicalParty || rows[0].politicalParty,       
-        req.body.candidateName || rows[0].candidateName,
-        req.body.offices || rows[0].offices,
         moment(new Date()),
-        req.params.id
+        req.params.id,
+        req.user.id
       ];
       const response = await db.query(updateOneQuery, values);
       return res.status(200).send(response.rows[0]);
@@ -96,15 +93,15 @@ const politicalParty  = {
     }
   },
   /**
-   * Delete A political Party 
+   * Delete A politicalParty 
    * @param {object} req 
    * @param {object} res 
    * @returns {void} return statuc code 204 
    */
   async delete(req, res) {
-    const deleteQuery = 'DELETE FROM politicalPartys WHERE id=$1 returning *';
+    const deleteQuery = 'DELETE FROM politicalPartys WHERE id=$1 AND returning *';
     try {
-      const { rows } = await db.query(deleteQuery, [req.params.id]);
+      const { rows } = await db.query(deleteQuery, [req.params.id, req.user.id]);
       if(!rows[0]) {
         return res.status(404).send({'message': 'politicalParty not found'});
       }
